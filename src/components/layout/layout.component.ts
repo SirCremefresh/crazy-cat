4131a2670c1eabd7b695b8404e4fb3e0d56163b8
SirCremefresh <donato.wolfisberg@gmail.com>
2019-05-27 15:32:17 +0200

    on disconnect

--- src/components/layout/layout.component.ts
+++ src/components/layout/layout.component.ts
@@ -3,17 +3,18 @@ import template from './layout.component.html'
 export class LayoutComponent extends HTMLElement {
     isMenuOpen = false;
     navigation: HTMLElement;
+    menuButton: HTMLButtonElement;
 
     constructor() {
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
-        
-        const menuButton = <HTMLButtonElement>this.shadowRoot.getElementById("menu-button");
+
+        this.menuButton = <HTMLButtonElement>this.shadowRoot.getElementById("menu-button");
         this.navigation = <HTMLElement>this.shadowRoot.getElementById("navigation");
 
-        menuButton.onclick = () => {
+        this.menuButton.onclick = () => {
             this.isMenuOpen = !this.isMenuOpen;
             this.setNavAttribute();
         };
@@ -23,6 +24,10 @@ export class LayoutComponent extends HTMLElement {
 
     private setNavAttribute() {
         this.navigation.setAttribute("data-open", `${this.isMenuOpen}`);
+    }
+
+    public disconnectedCallback() {
+        this.menuButton.onclick = undefined;
     }
 }
 
