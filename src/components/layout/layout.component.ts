abc443a714a8fa69f131ae27620fe8635dbbbef9
SirCremefresh <donato.wolfisberg@gmail.com>
2019-05-27 15:22:43 +0200

    added navigation button functionality

--- src/components/layout/layout.component.ts
+++ src/components/layout/layout.component.ts
@@ -1,13 +1,29 @@
 import template from './layout.component.html'
 
 export class LayoutComponent extends HTMLElement {
+    isMenuOpen = false;
+    navigation: HTMLElement;
+
     constructor() {
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
+        
+        const menuButton = <HTMLButtonElement>this.shadowRoot.getElementById("menu-button");
+        this.navigation = <HTMLElement>this.shadowRoot.getElementById("navigation");
+
+        menuButton.onclick = () => {
+            this.isMenuOpen = !this.isMenuOpen;
+            this.setNavAttribute();
+        };
+
+        this.setNavAttribute();
     }
 
+    private setNavAttribute() {
+        this.navigation.setAttribute("data-open", `${this.isMenuOpen}`);
+    }
 }
 
 customElements.define('app-layout', LayoutComponent);
