98c02b4a46f7f7cc39b982427bbd685f1b9ea49a
SirCremefresh <donato.wolfisberg@gmail.com>
2019-06-05 22:36:48 +0200

    added heart turning red on click

--- frontend/src/components/image/image.component.ts
+++ frontend/src/components/image/image.component.ts
@@ -2,12 +2,23 @@ import template from './image.component.html';
 
 export class ImageComponent extends HTMLElement {
     likes = Math.floor(Math.random() * 50);
+    likeIcon: HTMLElement;
+    likeButton: HTMLButtonElement;
 
     constructor() {
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
+
+        this.likeIcon = this.shadowRoot.getElementById("like-icon");
+        this.likeButton = <HTMLButtonElement>this.shadowRoot.getElementById("like-button");
+
+        this.likeButton.addEventListener('click', () => {
+            this.likeIcon.classList.add("liked");
+        });
+
+
     }
 
     public disconnectedCallback() {
