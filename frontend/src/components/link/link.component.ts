cc9728d259ae5931b3c1e29577c113fd090b3c30
SirCremefresh <donato.wolfisberg@gmail.com>
2019-06-06 10:49:00 +0200

    optimise lighthouse

--- frontend/src/components/link/link.component.ts
+++ frontend/src/components/link/link.component.ts
@@ -1,17 +1,15 @@
 import {navigateTo} from '../../router';
 
 export class LinkComponent extends HTMLAnchorElement {
-	connectedCallback() {
-		this.addEventListener('click', e => {
-			e.preventDefault();
+    connectedCallback() {
+        this.addEventListener('click', e => {
+            e.preventDefault();
 
-			console.log(this.pathname);
-
-			navigateTo(this.pathname);
-		});
-	}
+            navigateTo(this.pathname);
+        });
+    }
 }
 
 customElements.define('app-link', LinkComponent, {
-	extends: 'a'
+    extends: 'a'
 });
