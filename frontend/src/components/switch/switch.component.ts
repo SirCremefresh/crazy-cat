7c7f5951e9bcf60c3d10e7b704f4b8a43a815a17
SirCremefresh <donato.wolfisberg@gmail.com>
2019-06-07 23:30:08 +0200

    cleaned switch

--- frontend/src/components/switch/switch.component.ts
+++ frontend/src/components/switch/switch.component.ts
@@ -12,17 +12,27 @@ export class SwitchComponent extends HTMLElement {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
 
+        this.onOuterButtonClick = this.onOuterButtonClick.bind(this);
+
         this.outerButton = this.shadowRoot.querySelector(".outer");
         this.innerButton = this.shadowRoot.querySelector(".inner");
+    }
 
-        this.outerButton.addEventListener('click', () => {
-            if (this.enabled) {
-                this.innerButton.classList.add("disabled");
-            } else {
-                this.innerButton.classList.remove("disabled");
-            }
-            this.enabled = !this.enabled;
-        });
+    connectedCallback() {
+        this.outerButton.addEventListener('click', this.onOuterButtonClick);
+    }
+
+    onOuterButtonClick() {
+        if (this.enabled) {
+            this.innerButton.classList.add("disabled");
+        } else {
+            this.innerButton.classList.remove("disabled");
+        }
+        this.enabled = !this.enabled;
+    }
+
+    public disconnectedCallback() {
+        this.outerButton.removeEventListener('click', this.onOuterButtonClick);
     }
 }
 
