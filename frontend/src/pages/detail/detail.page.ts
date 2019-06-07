4982596d4bca6a0988b4a73c074dbd9fb6164de1
SirCremefresh <donato.wolfisberg@gmail.com>
2019-06-07 17:24:41 +0200

    made a baad implementation of detail page

--- frontend/src/pages/detail/detail.page.ts
+++ frontend/src/pages/detail/detail.page.ts
@@ -2,16 +2,11 @@ import template from './detail.page.html';
 import '../../components/imagedetail/imagedetail.component';
 
 export class DetailPage extends HTMLElement {
-    image: HTMLElement;
-
     constructor() {
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
-
-        this.image = this.shadowRoot.querySelector('#image');
-        this.image.append(document.createElement('app-imagedetail'));
     }
 }
 
