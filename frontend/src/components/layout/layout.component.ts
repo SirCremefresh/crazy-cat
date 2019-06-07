71c97c91c10dd6097ad6c9f318259f61daf015d1
Simon Kaspar <simon@kaspar-web.ch>
2019-06-07 09:55:10 +0200

    footer

--- frontend/src/components/layout/layout.component.ts
+++ frontend/src/components/layout/layout.component.ts
@@ -2,11 +2,17 @@ import template from './layout.component.html'
 import '../header/header.component'
 
 export class LayoutComponent extends HTMLElement {
+
+    footer: HTMLElement;
+
     constructor() {
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
+
+        this.footer = this.shadowRoot.querySelector("footer");
+        this.footer.innerHTML = new Date().getFullYear() + ' - JOSIDO dev';
     }
 }
 
