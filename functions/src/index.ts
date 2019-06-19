fb11547b0a3be324d17ca3686637f795041ce50e
SirCremefresh <donato.wolfisberg@gmail.com>
2019-06-19 20:35:46 +0200

    not working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import {ObjectMetadata} from "firebase-functions/lib/providers/storage";
-import {Readable, Writable} from "stream";
+import {Writable} from "stream";
 import * as path from "path";
 import * as os from "os";
 import * as fs from "fs";
@@ -42,7 +42,6 @@ exports.generateResponsiveContent = functions.storage.object().onFinalize(async 
 });
 
 async function handleVideoConversion(file: ObjectMetadata, filePath: string) {
-    const remoteReadStream = bucket.file(filePath).createReadStream();
 
     const tempFilePath = path.join(os.tmpdir(), filePath);
     await bucket.file(filePath).download({destination: tempFilePath});
@@ -51,12 +50,12 @@ async function handleVideoConversion(file: ObjectMetadata, filePath: string) {
 // Once the thumbnail has been uploaded delete the local file to free up disk space.
 
     await Promise.all([
-        uploadInSize(remoteReadStream, getWriteStream('s_' + filePath), 560),
+        uploadInSize(tempFilePath, getWriteStream('s_' + filePath), 560),
         // uploadInSize(remoteReadStream, getWriteStream('m_' + filePath), 960),
         // uploadInSize(remoteReadStream, getWriteStream('l_' + filePath), 1200)
     ]);
 
-    
+
     return fs.unlinkSync(tempFilePath);
 }
 
@@ -70,16 +69,15 @@ function getWriteStream(filePath: string) {
 }
 
 // ffmpeg -ss 00:00:10 -i philips_formula1.mkv -y -r 24 -s 1280x720 -c:v libx264 -b:v 2500k -c:a aac -b:a 128k
-function uploadInSize(readStream: Readable, writeStream: Writable, width: number) {
+function uploadInSize(readStream: string, writeStream: Writable, width: number) {
     return new Promise((resolve, reject) => {
-        ffmpeg()
-            .input(readStream)
+        ffmpeg(readStream)
             .fps(24)
             .audioCodec("aac")
             .videoCodec("libx264")
             .size(`${width}x?`).aspect('4:3').autopad('black')
             .format('mp4')
-            .outputOptions('-movflags +faststart')
+            // .outputOptions('-movflags +faststart')
             .on('start', (cmdLine: string) => {
                 console.log('Started ffmpeg with command:', cmdLine);
             })
