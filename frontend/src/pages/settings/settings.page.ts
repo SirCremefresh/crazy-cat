0251a2b3042f88b8c844176cf311ec68e489785d
Simon Kaspar <simon@kaspar-web.ch>
2019-06-21 14:04:24 +0200

    toggleable dark and pride modes
    button status is not correct atm when revisiting settings page

--- frontend/src/pages/settings/settings.page.ts
+++ frontend/src/pages/settings/settings.page.ts
@@ -1,12 +1,46 @@
 import template from './settings.page.html';
 import '../../components/switch/switch.component';
+import {styleService} from "../../api/style.service";
+import { SwitchComponent } from '../../components/switch/switch.component';
 
 export class HomePage extends HTMLElement {
+
+    prideSwitch: SwitchComponent;
+    darkSwitch: SwitchComponent;
+
     constructor() {
         super();
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = template;
+
+        this.prideSwitch = this.shadowRoot.querySelector('app-switch[data-js="prideSwitch"]');
+        this.darkSwitch = this.shadowRoot.querySelector('app-switch[data-js="darkSwitch"]');
+
+        this.togglePrideMode = this.togglePrideMode.bind(this);
+        this.toggleDarkMode = this.toggleDarkMode.bind(this);
+    }
+
+    connectedCallback() {
+        this.prideSwitch.addEventListener('click', this.togglePrideMode);
+        this.darkSwitch.addEventListener('click', this.toggleDarkMode);
+    }
+
+    togglePrideMode() {
+        if(this.prideSwitch.enabled){
+            styleService.setCurrentKey("Pride");
+        } else {
+            styleService.setCurrentKey("Default");
+        }
+    }
+
+    toggleDarkMode(){
+        if(this.darkSwitch.enabled){
+            styleService.setCurrentKey("Dark");
+        } else {
+            styleService.setCurrentKey("Default");
+        }
+        
     }
 }
 
